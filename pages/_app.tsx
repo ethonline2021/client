11a21366660c55935ccb3b74e85fb7110df2de4c
Òscar Casajuana <elboletaire@gmail.com>
2021-09-20 13:32:25 +0200

    Integrated antd, styled-components and livepeer example

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Layout, Menu, Breadcrumb } from 'antd'
+import { Content, Footer, Header } from 'antd/lib/layout/layout'
+
+import GlobalStyle from '../styles/globalStyles'
+
+import "antd/dist/antd.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <GlobalStyle />
+    <Layout>
+      <Header className="header">
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1">Home</Menu.Item>
+          <Menu.Item key="2">Creators</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
+          <Content style={{ padding: '0 24px', minHeight: 280 }}>
+            <Component {...pageProps} />
+          </Content>
+        </Layout>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Shadowy &amp; Òscar C. &copy;2021</Footer>
+    </Layout>
+  </>
 }
 export default MyApp
