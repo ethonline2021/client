96b0c25ba82d2dabed1905794faaae8320b3bcc4
Aleix <nigeon@gmail.com>
2021-10-07 16:15:17 +0200

    Redepolyed SC to Kovan

--- networks.ts
+++ networks.ts
@@ -14,9 +14,9 @@ const networks : Networks = {
   42: {
     key: 'kovan',
     title: 'Kovan testnet',
-    main: '0xa5b3080a12822A17288E78E7677f060E97d20F15',
+    main: '0x675f77951254a75D3196d094588F811d31E9fb94',
     payments: '0xe3cb950cb164a31c66e32c320a800d477019dcff',
-    graph: 'https://api.studio.thegraph.com/query/10173/ethonline2021_kovan/v0.0.13',
+    graph: 'https://api.studio.thegraph.com/query/10173/ethonline2021_kovan/v0.0.15',
   },
   137: {
     key: 'polygon',
