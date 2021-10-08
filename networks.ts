e7bcbf962f129973a9de7a3a506ad6f93f47b304
Aleix <nigeon@gmail.com>
2021-10-08 16:47:08 +0200

    New contracts

--- networks.ts
+++ networks.ts
@@ -14,9 +14,9 @@ const networks : Networks = {
   42: {
     key: 'kovan',
     title: 'Kovan testnet',
-    main: '0xe89420cc1753811b4E1b309D71dC49758e381322',
+    main: '0x332f949393a0528F4f32aD0aAB3D4EE57e62831B',
     payments: '0xe3cb950cb164a31c66e32c320a800d477019dcff',
-    graph: 'https://api.studio.thegraph.com/query/10173/ethonline2021_kovan/v0.0.21',
+    graph: 'https://api.studio.thegraph.com/query/10173/ethonline2021_kovan/v0.0.22',
   },
   137: {
     key: 'polygon',
