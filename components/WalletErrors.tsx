1bfea2a91749fb7c2a20d0ba51e622b78cdefebd
Òscar Casajuana <elboletaire@gmail.com>
2021-10-09 23:30:23 +0200

    WalletErrors improvements

--- components/WalletErrors.tsx
+++ components/WalletErrors.tsx
@@ -1,15 +1,29 @@
 import { useWeb3React } from "@web3-react/core"
+import { Alert } from "antd"
+import { useEffect } from "react"
 import { useErrors } from "../providers"
 
 const WalletErrors = () => {
-  const { error: walletError } = useErrors()
+  const { error: walletError, setError } = useErrors()
   const { error } = useWeb3React()
+
+  useEffect(() => {
+    if (walletError && !error) {
+      setTimeout(() => {
+        setError('')
+      }, 5000)
+    }
+  }, [walletError, error])
 
   if (!error && !walletError) {
     return null
   }
 
-  return <p style={{color: 'red'}}>{(error && error.message) || walletError}</p>
+  return <Alert
+    type='error'
+    showIcon
+    message={(error && error.message) || walletError}
+  />
 }
 
 export default WalletErrors
