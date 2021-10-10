d8796e08d41be40833a0f14f2face0acf40a452c
Aleix <nigeon@gmail.com>
2021-10-10 09:29:53 +0200

    NEW SC and subgraph

--- networks.ts
+++ networks.ts
@@ -14,9 +14,9 @@ const networks : Networks = {
   42: {
     key: 'kovan',
     title: 'Kovan testnet',
-    main: '0x61A04FBc38757BC0c75e1Be8edF7cA757C5aD422',
+    main: '0x1EC422B81f33F5FEb1CADf0F19aCD6988dc1d873',
     payments: '0xe3cb950cb164a31c66e32c320a800d477019dcff',
-    graph: 'https://api.studio.thegraph.com/query/10173/ethonline2021_kovan/v0.1',
+    graph: 'https://api.studio.thegraph.com/query/10173/ethonline2021_kovan/v0.1.2',
   },
   137: {
     key: 'polygon',
@@ -25,9 +25,9 @@ const networks : Networks = {
   80001: {
     key: 'mumbai',
     title: 'Mumbai testnet',
-    main: '0x213D0201767E79F6896326528Fc9C65Bfef7CbC1',
+    main: '0x240C51c33077da7447550adAaafF7643150D5Fcc',
     payments: '0x5D8B4C2554aeB7e86F387B4d6c00Ac33499Ed01f',
-    graph: 'https://api.studio.thegraph.com/query/10173/ethonline2021/v0.1',
+    graph: 'https://api.studio.thegraph.com/query/10173/ethonline2021/v0.1.2',
   },
 }
 
